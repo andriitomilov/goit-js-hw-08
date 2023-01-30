4c0b8a14838ad04d394ef78ed55166ae169a27b1
andriitomilov <andriitomilov@ukr.net>
2023-01-30 11:12:22 +0200

    add lodash/vimeo and code to task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const currentTime = {
+  'videoplayer-current-time': 0,
+};
+
+player.getCurrentTime(currentTime['videoplayer-current-time']);
+
+player.on(
+  'timeupdate',
+  throttle(function (e) {
+    currentTime['videoplayer-current-time'] = e.seconds;
+    console.log(currentTime);
+  }, 1000)
+);
